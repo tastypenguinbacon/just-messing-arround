053e4fea2eb8a57ff080f493c5e84ba1ebb2e052
pingwin <jaloszewski.adrian@gmail.com>
2017-03-01 23:15:13 +0100

    Added build to gitignore

---
+++ src/main/java/io/tastypenguinbacon/cheat/Application.java
@@ -0,0 +1,14 @@
+package io.tastypenguinbacon.cheat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by pingwin on 01.03.17.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
