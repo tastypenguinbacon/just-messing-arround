d42d9e27fca0b3f3c4f0aad242541538a58ef577
pingwin <jaloszewski.adrian@gmail.com>
2017-03-18 23:21:14 +0100

    added log4j

--- java/string-util/src/main/java/com/github/tastypenguinbacon/string/Application.java
+++ java/string-util/src/main/java/com/github/tastypenguinbacon/string/Application.java
@@ -1,5 +1,7 @@
-package com.github.tastypenguinbacon.cheat;
+package com.github.tastypenguinbacon.string;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Application {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String... args) {
+        logger.error("Hello world");
         SpringApplication.run(Application.class, args);
     }
 }
