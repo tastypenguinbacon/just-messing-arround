4051e3288ea77a1beff834a4c0fc19c234aa836b
pingwin <jaloszewski.adrian@gmail.com>
2017-08-02 21:49:29 +0200

    cleared the repository

--- haskell/scratch/zipWith.hs
+++
@@ -1,8 +0,0 @@
-zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
-zipWith' _ [] _ = []
-zipWith' _ _ [] = []
-zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
-
-main :: IO()
-main = do
-  print $ zipWith' (+) [1 ..] [1 .. 10]
--- python/scratch/just_having_fun.py
+++
@@ -1,5 +0,0 @@
-generator = (x ** 2 for x in range(1000) if x % 2 == 0)
-
-
-for x in generator:
-    print(x)
