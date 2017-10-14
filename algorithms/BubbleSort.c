c352ed90daf4cf687ba053fa7b9ed22d0ab62303
pingwin <jaloszewski.adrian@gmail.com>
2017-10-14 16:15:32 +0200

    two sorting algorithms

---
+++ algorithms/BubbleSort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum {
+    False, 
+    True
+} bool;
+
+static void swap(int *begin, int *end) {
+    int c = *begin;
+    *begin = *end;
+    *end = c;
+}
+
+void bubble_sort(int *begin, int* end) {
+    bool bubbled = True;
+    while (bubbled) {
+        bubbled = False;
+        for (int i = 1; begin + i != end; i++) {
+            if (begin[i] < begin[i - 1]) {
+                bubbled = True;
+                swap(&begin[i - 1], &begin[i]);
+            } 
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    const int number_count = argc - 1;
+    int *table = malloc(sizeof(int) * number_count);
+
+    for (int i = 0; i < number_count; i++) {
+        table[i] = atoi(argv[i + 1]);
+    }
+
+    bubble_sort(table, table + number_count);
+
+    for (int i = 0; i < number_count; i++) {
+        printf("%d ", table[i]);
+    }
+    putchar('\n');
+
+    free(table);
+    return 0;
+}
