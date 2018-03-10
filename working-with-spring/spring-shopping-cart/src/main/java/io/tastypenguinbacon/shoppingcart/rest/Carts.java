2002ba91fbd01e9f68fb3cdd4c1b004cef488943
pingwin <jaloszewski.adrian@gmail.com>
2018-03-10 21:37:47 +0100

    Basic spring app

--- working-with-spring/spring-shopping-cart/src/main/java/io/tastypenguinbacon/shoppingcart/rest/Carts.java
+++ working-with-spring/spring-shopping-cart/src/main/java/io/tastypenguinbacon/shoppingcart/rest/Carts.java
@@ -1,7 +1,15 @@
 package io.tastypenguinbacon.shoppingcart.rest;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by pingwin on 04.01.18.
  */
+@RestController
+@RequestMapping(value = "carts",
+        consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+        produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class Carts {
 }
