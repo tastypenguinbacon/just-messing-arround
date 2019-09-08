90f59025f621f38ca56693057e32d294a1f39cb0
pingwin <jaloszewski.adrian@gmail.com>
2019-09-08 10:13:29 +0200

    Mandelbrot with args

--- algo/fractal/python-mandelbrot.py
+++ algo/fractal/python-mandelbrot.py
@@ -1,8 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from argparse import ArgumentParser
 
-x = np.linspace(-1.41, -1.4075, 2000)
-y = np.linspace(0.1375, 0.135, 1500)
+parser = ArgumentParser()
+parser.add_argument('-minRe', type=float, default=-1.41)
+parser.add_argument('-maxRe', type=float, default=-1.4075)
+parser.add_argument('-minIm', type=float, default=0.135)
+parser.add_argument('-maxIm', type=float, default=0.1375)
+parser.add_argument('-sharpness', type=int, default=2000)
+args = parser.parse_args()
+
+tan = abs(args.maxIm - args.minIm) / abs(args.maxRe - args.minRe)
+
+if tan <= 1:
+    x_elem_cnt = args.sharpness
+    y_elem_cnt = int(x_elem_cnt * tan)
+else:
+    y_elem_cnt = args.sharpness
+    x_elem_cnt = int(x_elem_cnt / tan)
+
+
+x = np.linspace(args.minRe, args.maxRe, x_elem_cnt)
+y = np.linspace(args.maxIm, args.minIm, y_elem_cnt)
 
 c = np.repeat(np.array([x]), y.size, axis=0) + np.repeat(np.array([y]), x.size, axis=0).T * 1j
 z = np.zeros(c.shape, dtype=np.complex)
